055903f80dfe0c37532ff036cfd0ab67110e3fdd
hu <hucgqg@qq.com>
2022-01-08 10:41:14 +0800

    update file

--- 02_src_reactive方式修改数据/App.vue
+++ 02_src_reactive方式修改数据/App.vue
@@ -1,16 +1,19 @@
 <template>
     <h4>个人信息</h4>
-    <h6>姓名:{{ person.name }}</h6>
+    <h6 v-show="person.name">姓名:{{ person.name }}</h6>
     <h6>年龄:{{ person.age }}</h6>
+    <h6 v-show="person.sex">性别:{{ person.sex }}</h6>
     <h6>岗位:{{ person.job.type }}</h6>
     <h6>薪资:{{ person.job.salary }}</h6>
     <h6>爱好:{{ person.hobby }}</h6>
     <button @click="sayHello">自我介绍</button>
     <button @click="updateInfo">更新数据</button>
+    <button @click="addSex">添加性别</button>
+    <button @click="delName">删除姓名</button>
 </template>
 
 <script>
-import { reactive, ref } from "vue";
+import { reactive } from "vue";
 export default {
     name: "App",
     setup() {
@@ -35,10 +38,18 @@ export default {
             person.job.salary = "50k";
             person.hobby[2] = "喝水";
         }
+        function addSex() {
+            person.sex = "男";
+        }
+        function delName() {
+            delete person.name;
+        }
         return {
             person,
             sayHello,
             updateInfo,
+            addSex,
+            delName,
         };
     },
 };
